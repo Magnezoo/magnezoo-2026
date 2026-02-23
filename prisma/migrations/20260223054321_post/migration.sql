ab7f570d2c9b0ed77d7eff054701a1525b809caa
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-02-23 17:51:49 +0900

    ポストテーブルの追加

---
+++ prisma/migrations/20260223054321_post/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "post" (
+    "id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT NOT NULL,
+    "isSalesApplication" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "post_title_idx" ON "post"("title");
+
+-- CreateIndex
+CREATE INDEX "post_isSalesApplication_idx" ON "post"("isSalesApplication");
+
+-- CreateIndex
+CREATE INDEX "post_createdAt_idx" ON "post"("createdAt");
+
+-- AddForeignKey
+ALTER TABLE "post" ADD CONSTRAINT "post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
