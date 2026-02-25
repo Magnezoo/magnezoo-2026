77026d40e0d9644246a00be1385dfb2a312ef735
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-02-25 22:17:09 +0900

    @next/third-partiesを追加し、Google Analyticsをレイアウトに統合

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <GoogleAnalytics gaId={"G-C93T9DHG9G"} />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
