282775dd5e6a785097e3720e63d838f246b25862
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-09 15:04:48 +0900

    miss

--- src/components/Dialogs/PostDetail/index.tsx
+++ src/components/Dialogs/PostDetail/index.tsx
@@ -35,7 +35,7 @@ export default async function PostDetailDialog(props: PostDetailDialogProps) {
             },
             votes: {
               where: {
-                isSalesApplication: true,
+                isSalesApplication: false,
               },
               select: {
                 userId: true,
