2b4514e8a330a361f2e0eba7bcebc5c9e367e878
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-10 23:12:34 +0900

    OGPを動的生成して投稿ページに対してもOGPを表示する

--- src/app/(user)/(posts)/posts/[id]/page.tsx
+++ src/app/(user)/(posts)/posts/[id]/page.tsx
@@ -1,7 +1,73 @@
 import { Stack, Typography } from "@mui/material";
+import type { Metadata } from "next";
+import { headers } from "next/headers";
 import { Suspense } from "react";
 import PostDetailDialog from "@/components/Dialogs/PostDetail";
+import prisma from "@/lib/prisma";
 import ResolvedPostsPage from "../using";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const post = await prisma.post.findUnique({
+    where: { id },
+    select: {
+      title: true,
+      description: true,
+      imageUrl: true,
+      tags: {
+        select: { tag: { select: { name: true } } },
+      },
+      author: {
+        select: {
+          slacks: {
+            select: { name: true, isDisplayname: true },
+          },
+          nickName: true,
+        },
+      },
+      createdAt: true,
+    },
+  });
+  if (!post) {
+    return {
+      title: "投稿が見つかりませんでした",
+      description: `投稿ID: ${id}の詳細ページは存在しません。`,
+    };
+  }
+  const header = await headers();
+  const protocol = header.get("x-forwarded-proto") || "http";
+  const host =
+    header.get("host") || header.get("x-forwarded-host") || "localhost:3000";
+  const tags = post.tags.map((t) => t.tag.name);
+  const authorName =
+    post.author.nickName ||
+    post.author.slacks.find((s) => s.isDisplayname)?.name ||
+    post.author.slacks[0]?.name ||
+    "匿名ユーザー";
+  return {
+    title: `${post.title}`,
+    description: `${post.description.slice(0, 100)}...`,
+    openGraph: {
+      title: `${post.title}`,
+      description: `${post.description.slice(0, 100)}...`,
+      images: `${protocol}://${host}${post.imageUrl}`,
+      type: "article",
+      tags,
+      authors: [authorName],
+      publishedTime: post.createdAt.toISOString(),
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${post.title}`,
+      description: `${post.description.slice(0, 100)}...`,
+      images: `${protocol}://${host}${post.imageUrl}`,
+    },
+  };
+}
 
 export default async function PostDetailPage({
   params,
