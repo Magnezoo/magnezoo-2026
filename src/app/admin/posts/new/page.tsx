3bfaed215a1f20f66c6119455fbe13062e2f991c
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-17 21:40:38 +0900

    miss

--- src/app/admin/posts/new/page.tsx
+++ src/app/admin/posts/new/page.tsx
@@ -1,5 +1,7 @@
+import prisma from "@/lib/prisma";
 import PostEditForm from "../[id]/edit/PostEditForm";
 
-export default function NewPostPage() {
-  return <PostEditForm />;
+export default async function NewPostPage() {
+  const users = await prisma.user.findMany();
+  return <PostEditForm users={users} />;
 }
